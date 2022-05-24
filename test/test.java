f4f44109255eeb764a971960088a7ac3166561c4
MoW2003 <shahrukhc2003@hotmail.com>
2022-05-24 00:33:34 -0300

    welcome message

---
+++ test/test.java
@@ -0,0 +1,16 @@
+package test;
+
+public class test {
+    public static void main(String[] args) {
+        String[] message = {
+            "              _",
+            "__      _____| | ___ ___  _ __ ___   ___",
+            "\\ \\ /\\ / / _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\",
+            " \\ V  V /  __/ | (_| (_) | | | | | |  __/",
+            "  \\_/\\_/ \\___|_|\\___\\___/|_| |_| |_|\\___|"
+        };
+        for (String string : message) {
+            System.out.println(string);
+        }
+    }
+}
