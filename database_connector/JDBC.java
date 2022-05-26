3d65230854d6c6b55e86737f38ef60fbee99502f
MoW2003 <shahrukhc2003@hotmail.com>
2022-05-26 00:35:06 -0300

    inserUser now works & added some more methods

--- database_connector/JDBC.java
+++ database_connector/JDBC.java
@@ -3,13 +3,55 @@ package database_connector;
 import java.sql.*;
 
 public class JDBC {
+    // public void getUser() {
+    //     Connection conn2 = getConnection();
+    //     try {
+    //         Statement statement = conn.createStatement();
+    //         ResultSet results = statement.executeQuery(sql)
+    //     }
+    //     catch (Exception e) {
+    //         System.out.println();
+    //     }
+    // }
+
+    // public void writeUser() {
+    //     Connection conn = getConnection();
+    //     try {
+            
+    //     }
+    //     catch (Exception e) {
+    //         System.out.println();
+    //     }
+    // }
+
+    public void insertUsers(String username, String birthdate, String password) throws SQLException {
+        Connection conn = getConnection();
+        try {
+            Statement statement = conn.createStatement();
+            // Menu dat = new Menu();
+            // Menu.userData();
+            // String password = "h345433u";
+            // String username = "wfd5434";
+            // String birthdate = "2000-01-01";
+            String query = ("INSERT INTO players (username, birthdate, password) "
+                + "VALUES ('"+ username + "', '" + birthdate + "', '" + password +"')");
+            statement.executeUpdate(query);
+            System.out.println("Registration successful.");
+        }
+        catch (Exception e) {
+            System.out.println();
+            System.out.println("Please try another username.");
+        }
+        conn.close();
+    }
+
     public void printUsers() throws SQLException {
         Connection conn = getConnection();
         try {
             Statement statement = conn.createStatement();
             ResultSet results =  statement.executeQuery("SELECT username, score FROM scoreboard " 
-                                    + "INNER JOIN players ON scoreboard.id_players=players.id_players;");
-            System.out.println();
+                            + "INNER JOIN players ON scoreboard.id_players=players.id_players;");
+
             while(results.next()) {
                 //String id = results.getString("id_players");
                 String username = results.getString("username");
@@ -17,7 +59,8 @@ public class JDBC {
                 System.out.println(username + " - " + score);
             }
             System.out.println();
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             System.out.println("Could not retrieve data from the database " + e.getMessage());
         }
         conn.close();
