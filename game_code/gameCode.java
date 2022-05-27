39b58ac0ea29a2add06dd32ef75941b1a4118736
MoW2003 <shahrukhc2003@hotmail.com>
2022-05-27 19:24:15 -0300

    added some gameCode

--- game_code/gameCode.java
+++ game_code/gameCode.java
@@ -1,8 +1,30 @@
 package game_code;
 
+import java.util.Random;
+
 // import java.util.Random;
 // import java.util.Scanner;
 
 public class gameCode {
-    
+
+    static String[][] cardsUp = new String[4][5];
+    static String[][] cardsDown = new String[4][5];
+
+
+
+// public static void printBord()
+
+
+
+    public static String[][] shuffle() {
+        String letters[] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+                            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+        String cards[][] = new String[4][5];
+        Random random =  new Random();
+        for (int i = 0; i < 19; ) {
+            int rnd = random.nextInt(letters.length);
+            System.out.println(letters[rnd]);
+        }
+        return cards;
+    }
 }
