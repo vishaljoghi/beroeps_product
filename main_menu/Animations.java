5c57e7dfba0c00fdd80ba4f027be575f234519e9
MoW2003 <shahrukhc2003@hotmail.com>
2022-05-29 12:04:47 -0300

    moved animations to sepparate file

---
+++ main_menu/Animations.java
@@ -0,0 +1,66 @@
+package main_menu;
+
+public class Animations {
+    public static void welcomeMessage() throws InterruptedException {
+        // String[] welcome = {
+        //     " ",
+        //     "              _",
+        //     "__      _____| | ___ ___  _ __ ___   ___",
+        //     "\\ \\ /\\ / / _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\",
+        //     " \\ V  V /  __/ | (_| (_) | | | | | |  __/",
+        //     "  \\_/\\_/ \\___|_|\\___\\___/|_| |_| |_|\\___|",
+        //     " ",
+        //     " "
+        // };
+        // for (String a : welcome) {
+        //     System.out.println(a);
+        // }
+        // Thread.sleep(5000);
+
+        String[] memory = {
+            " ",
+            " ",
+            " _ __ ___   ___ _ __ ___   ___  _ __ _   _",
+            "| '_ ` _ \\ / _ \\ '_ ` _ \\ / _ \\| '__| | | |",
+            "| | | | | |  __/ | | | | | (_) | |  | |_| | ",
+            "|_| |_| |_|\\___|_| |_| |_|\\___/|_|   \\__, |",
+            "                                     |___/",
+            " ",
+            " "
+        };
+        for (String b : memory) {
+            System.out.println(b);
+        }
+        Thread.sleep(2000);
+        String[] impossible = {
+            " _                               _ _     _",
+            "(_)_ __ ___  _ __   ___  ___ ___(_) |__ | | ___",
+            "| | '_ ` _ \\| '_ \\ / _ \\/ __/ __| | '_ \\| |/ _ \\",
+            "| | | | | | | |_) | (_) \\__ \\__ \\ | |_) | |  __/ ",
+            "|_|_| |_| |_| .__/ \\___/|___/___/_|_.__/|_|\\___|",
+            "            |_|",
+            " "
+        };
+        for (String c : impossible) {
+            System.out.println(c);
+        }
+        Thread.sleep(2000);
+    }
+
+    static void menuExeption() {
+        String[] error = {
+            "       _   ___   _ ",
+            "      | | |__ \\ | |",
+            "      | |   / / | |",
+            "      |_|  |_|  |_|",
+            "      (_)  (_)  (_)",
+            " ",
+            "Pls enter an option number.",
+            " "
+        };
+        for (String i : error) {
+            System.out.println(i);
+        }
+        Menu.enterReturn();
+    }
+}
