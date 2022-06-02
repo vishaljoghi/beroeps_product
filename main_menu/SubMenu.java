f4906432ae848c34a52dc74a55b67c4bb8ddc2fe
MoW2003 <shahrukhc2003@hotmail.com>
2022-06-02 20:11:09 -0300

    removed forgot acc and changed edit acc

--- main_menu/SubMenu.java
+++ main_menu/SubMenu.java
@@ -1,11 +1,13 @@
 package main_menu;
 
-import database_connector.*;
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import database_connector.JDBC;
+
 public class SubMenu {
-    public void manageAccount() {
+    protected void manageAccount() {
         String[] options = {
             " ",
             "   Manage Account",
@@ -13,8 +15,7 @@ public class SubMenu {
             "1. Switch account",
             "2. Create Account",
             "3. Edit Account",
-            "4. Forgot Acc",
-            "5. Return",
+            "4. Return",
             " "
         };
         int option;
@@ -25,35 +26,30 @@ public class SubMenu {
             System.out.print("Choose your option: ");
             Scanner answer = new Scanner(System.in);
             Menu m = new Menu();
-            JDBC j = new JDBC();
+            LoginMenu l = new LoginMenu();
             try {
                 option = answer.nextInt();
                 switch (option) {
                     case 1:
-                        j.getUser();
+                        l.getCred();
                         m.mainMenu();
                         break;
                     case 2:
-                        j.insertUsers();
+                        l.enterCred();
                         Menu.enterContinue();
                         break;
                     case 3:
-                        j.editUser();
-                        j.getUser();
+                        editCred();
                         m.mainMenu();
                         break;
                     case 4:
-                        lostAcc();
-                        Menu.enterReturn();
-                        break;
-                    case 5:
                         m.mainMenu();
                         break;
                 }
                 
             }
             catch (InputMismatchException e) {
-                System.out.println("Exeption: " + e);
+                System.out.println("InputMismatchException in subMenu: " + e);
                 Animations.menuExeption();
                 manageAccount();
             }
@@ -65,40 +61,108 @@ public class SubMenu {
         
     }
 
-    public void editCred() {
+    protected void editCred() {
         Scanner userCred = new Scanner(System.in);
-        String[] i = {
-            "New username: ",
-            "New birthdate: ",
-            "New password: "
-        };
-        try {
-            System.out.println(i[0]);
-            LoginMenu.setDbUsername(userCred.nextLine());
-            System.out.println(i[1]);
-            LoginMenu.setDbBirthdate(userCred.nextLine());
-            System.out.println(i[2]);
-            LoginMenu.setDbPassword(userCred.nextLine());
-        }
-        catch (Exception e) {
-            System.out.println("exeption in subMenu.editData: " + e);
-            // e.printStackTrace();
-        }
-    }
-
-    static void lostAcc() {
-        String[] message = {
+        String[] options = {
             " ",
-            "       __",
-            "  _   / /",
-            " (_) | |   Pls make another",
-            "  _  | |       account.",
-            " (_) | |      ----------",
-            "      \\_\\",
+            "     Account details",
+            "-------------------------",
+            "1. Username: " + LoginMenu.getUsername(),
+            "2. Birthdate: " + LoginMenu.getBirthdate(),
+            "3. Password: " + LoginMenu.getPassword(),
+            "4. Return",
             " "
         };
-        for (String i : message) {
-            System.out.println(i);
+        int option;
+        for (String i : options) {
+        System.out.println(i);
         }
+        System.out.print("Choose your option: ");
+        Scanner answer = new Scanner(System.in);
+        JDBC j = new JDBC();
+        try {
+            option = answer.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.print("\r\n" + "New username: ");
+                    LoginMenu.setDbUsername(userCred.nextLine());
+                    try {
+                        LoginMenu.setBirthdate(LoginMenu.getDbBirthdate());
+                        LoginMenu.setPassword(LoginMenu.getDbPassword());
+                        j.editUser();
+                    }
+                    catch (SQLIntegrityConstraintViolationException e) {
+                        System.out.println("\r\n" + "Username already taken." + "\r\n");
+                        Menu.enterContinue();
+                        editCred();
+                    }
+                    catch (Exception e) {
+                        System.out.println("exeption in subMenu.editCred: " + e);
+                        // e.printStackTrace();
+                    }
+                    LoginMenu.setUsername(LoginMenu.getDbUsername());
+                    editCred();
+                    break;
+                case 2:
+                    System.out.print("\r\n" + "New birthdate: ");
+                    LoginMenu.setDbBirthdate(userCred.nextLine());
+                    try {
+                        LoginMenu.setUsername(LoginMenu.getDbUsername());
+                        LoginMenu.setPassword(LoginMenu.getDbPassword());
+                        j.editUser();
+                    }
+                    catch (Exception e) {
+                        System.out.println("\r\n" + "\r\n" + "Incorrect date or format." + "\r\n" 
+                                + "Format: yyyy-mm-dd" + "\r\n" + "   e.g: 2000-12-30" + "\r\n");
+                        Menu.enterContinue();
+                        editCred();
+                    }
+                    LoginMenu.setBirthdate(LoginMenu.getDbBirthdate());
+                    editCred();
+                    break;
+                case 3:
+                    System.out.print("\r\n" + "New password: ");
+                    LoginMenu.setDbPassword(userCred.nextLine());
+                    try {
+                        LoginMenu.setUsername(LoginMenu.getDbUsername());
+                        LoginMenu.setBirthdate(LoginMenu.getDbBirthdate());
+                        j.editUser();
+                    }
+                    finally {}
+                    LoginMenu.setPassword(LoginMenu.getDbPassword());
+                    editCred();
+                    break;
+                case 4:
+                    manageAccount();
+                    break;
+            }
+        }
+        catch (InputMismatchException e) {
+            System.out.println("InputMismatchException in subMenu.editCred: " + e);
+            Animations.menuExeption();
+            editCred();
+        }
+        catch (Exception e) {
+            System.out.println("Exeption in subMenu.editCred: " + e);
+            Menu.enterReturn();
+            editCred();
+        }
+        
     }
+
+    // private void lostAcc() {
+    //     String[] message = {
+    //         " ",
+    //         "       __",
+    //         "  _   / /",
+    //         " (_) | |   Pls make another",
+    //         "  _  | |       account.",
+    //         " (_) | |      ----------",
+    //         "      \\_\\",
+    //         " "
+    //     };
+    //     for (String i : message) {
+    //         System.out.println(i);
+    //     }
+    // }
 }
